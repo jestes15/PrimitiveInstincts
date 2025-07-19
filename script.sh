bdcfc053f8269798bd8c0f657cb18707a908069e
Joshua E <estesjn2020@gmail.com>
2025-07-18 22:12:23 -0400

    Adding testing harness

---
+++ script.sh
@@ -0,0 +1,28 @@
+# header-begin ------------------------------------------
+# File       : script.sh
+#
+# Author      : Joshua E
+# Email       : estesjn2020@gmail.com
+#
+# Created on  : 7/18/2025
+#
+# header-end --------------------------------------------
+
+#!/bin/bash
+
+source /apps/profiles/modules_asax.sh.dyn
+
+module load oneapi
+module load cuda
+module load gcc/14.2.0
+
+cp ASA/Makefile .
+
+make
+
+LD_LIBRARY_PATH=/home/aubjne001/opencv_install/lib64:/apps/x86-64/apps/intel_2023.1.0/ipp/latest/lib/intel64:$LD_LIBRARY_PATH --config-file off --export /home/aubjne001/4k_profile --force-overwrite --set full --import-source yes main ./main
+
+# footer-begin ------------------------------------------
+# default.Shell
+# File       : script.sh
+# footer-end --------------------------------------------
